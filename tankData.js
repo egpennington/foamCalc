a27b930b67bd64d2ec9fa118d29ba16e8da51fa8
egpennington (애멧) <53251137+egpennington@users.noreply.github.com>
2025-09-30 09:51:13 -0800

    Update tankData.js
    add hydrant numbers

--- tankData.js
+++ tankData.js
@@ -1,15 +1,15 @@
 const tankData = {
-    10: { diameter: 95, area: 7088, capacity: 50000, material: "LSR", containment: 144000, location: "C st and 5th st", hydrant: "#20 on C st.", roof: "Cone/IFR" },
-    11: { diameter: 95, area: 7088, capacity: 50000, material: "Crude Oil", containment: 144000, location: "5th st", roof: "Cone/IFR" },
-    12: { diameter: 219, area: 37688, capacity: 300000, material: "Jet A", containment: 430000, location: "G st", roof: "Cone/IFR"},
-    13: { diameter: 219, area: 37688, capacity: 300000, material: "Crude Oil", containment: 430000, location: "G st", roof: "Cone/IFR" },
-    14: { diameter: 219, area: 37688, capacity: 300000, material: "Crude Oil", containment: 540000, location: "I st", roof: "Cone/IFR" },
-    20: { diameter: 200, area: 31416, capacity: 223000, material: "Low Sulfur VTB/LCO", containment: 309000, location: "C st and 5th st", roof: "Cone" },
-    22: { diameter: 134, area: 14103, capacity: 100000, material: "Diesel/Jet", containment: 190000, location: "E st and 5th st", roof: "Cone" },
-    23: { diameter: 219, area: 37688, capacity: 300000, material: "HVGO", containment: 430000, location: "H st and 5th st", roof: "Cone" },
-    24: { diameter: 95, area: 7088, capacity: 50000, material: "AGO", containment: 205000, location: "G st and 5th st", roof: "Cone" },
-    25: { diameter: 252, area: 49876, capacity: 425000, material: "Hi Sulfur Vac Bottoms", containment: 540000, location: "I st", roof: "Cone" },
-    30: { diameter: 42.5, area: 1419, capacity: 10000, material: "DF2/Jet A", containment: 167000, location: "A st and 4th st", roof: "Cone" },
+    10: { diameter: 95, area: 7088, capacity: 50000, material: "LSR", containment: 144000, location: "C st and 5th st", hydrant: "#20 on C st", roof: "Cone/IFR" },
+    11: { diameter: 95, area: 7088, capacity: 50000, material: "Crude Oil", containment: 144000, location: "5th st", hydrant: "#37 on 5th st", roof: "Cone/IFR" },
+    12: { diameter: 219, area: 37688, capacity: 300000, material: "Jet A", containment: 430000, location: "G st", hydrant: "#39 on G st/#48 on H st", roof: "Cone/IFR"},
+    13: { diameter: 219, area: 37688, capacity: 300000, material: "Crude Oil", containment: 430000, location: "G st", hydrant: "#40 pn G st/#41 on 6th st/#47 on H st", roof: "Cone/IFR" },
+    14: { diameter: 219, area: 37688, capacity: 300000, material: "Crude Oil", containment: 540000, location: "I st",  hydrant: "#42 on 6h st/#43 on I/#47 on H st", roof: "Cone/IFR" },
+    20: { diameter: 200, area: 31416, capacity: 223000, material: "Low Sulfur VTB/LCO", containment: 309000, location: "C st and 5th st", hydrant: "#8/9 on B st/#19/20 on C st", roof: "Cone" },
+    22: { diameter: 134, area: 14103, capacity: 100000, material: "Diesel/Jet", containment: 190000, location: "E st and 5th st", hydrant: "#36/37 on 5th st", roof: "Cone" },
+    23: { diameter: 219, area: 37688, capacity: 300000, material: "HVGO", containment: 430000, location: "H st and 5th st", hydrant: "#38 on G st/49 on H st/50 and 5th st", roof: "Cone", foam: "semi-fixed: 4115 gpm" },
+    24: { diameter: 95, area: 7088, capacity: 50000, material: "AGO", containment: 205000, location: "G st and 5th st", hydrant: "#34/35 on G st/50 on 5th st", roof: "Cone" },
+    25: { diameter: 252, area: 49876, capacity: 425000, material: "Hi Sulfur Vac Bottoms", containment: 540000, location: "I st", hydrant: "#48 H st/#44 on I st", roof: "Cone" },
+    30: { diameter: 42.5, area: 1419, capacity: 10000, material: "DF2/Jet A", containment: 167000, location: "A st and 4th st", hydrant: "#7 on 4th st/#6 on A st", roof: "Cone" },
     31: { diameter: 52, area: 2124, capacity: 15000, material: "DF2/DF1", containment: 167000, location: "A st", roof: "Cone" },
     32: { diameter: 112, area: 9852, capacity: 70000, material: "DF2/Jet A", containment: 167000, location: "B st and 4th st", roof: "Cone" },
     33: { diameter: 52, area: 2124, capacity: 15000, material: "Bunker/VTB", containment: 46400, location: "B st and 4th st", roof: "Cone"  },
@@ -21,10 +21,10 @@ const tankData = {
     42: { diameter: 85, area: 5675, capacity: 5675, material: "Asphalt", containment: 46400, location: "B st", roof: "Cone" },
     45: { diameter: 134, area: 14103, capacity: 100000, material: "Naphtha", containment: 190000, location: "G st and 3rd st", roof: "Cone/IFR" },
     51: { diameter: 60, area: 2827, capacity: 20000, material: "Asphalt", containment: 46400, location: "B st", roof: "Cone" },
-    60: { diameter: 52, area: 2124, capacity: 15000, material: "Diesel", containment: 89000, location: "A st and 1st st", hydrant: "#4 A st.", roof: "IFR" },
+    60: { diameter: 52, area: 2124, capacity: 15000, material: "Diesel", containment: 89000, location: "A st and 1st st", hydrant: "#11 B st.", roof: "IFR" },
     61: { diameter: 42.5, area: 1419, capacity: 10000, material: "Gasoline", containment: 89000, location: "B st and 1st st", hydrant: "#11 B st.", roof: "IFR" },
-    62: { diameter: 95, area: 7088, capacity: 50000, material: "Gasoline", containment: 89000, location: "A st and 1st st", roof: "Cone/IFR" },
-    63: { diameter: 95, area: 7088, capacity: 50000, material: "Gasoline", containment: 89000, location: "B st and 1st st", roof: "Cone/IFR" },
+    62: { diameter: 95, area: 7088, capacity: 50000, material: "Gasoline", containment: 89000, location: "A st and 1st st", hydrant: "#3 on A st.", roof: "Cone/IFR" },
+    63: { diameter: 95, area: 7088, capacity: 50000, material: "Gasoline", containment: 89000, location: "B st and 1st st", hydrant: "#12 on B st.", roof: "Cone/IFR" },
     64: { diameter: 134, area: 14103, capacity: 100000, material: "Unleaded/ Jet A", containment: 190000, location: "E st and 3rd st", roof: "Cone/IFR" },
     65: { diameter: 134, area: 14103, capacity: 100000, material: "UnLeaded Gasoline", containment: 205000, location: "G st and 3rd st", roof: "Cone/IFR" },
     66: { diameter: 134, area: 14103, capacity: 100000, material: "Sour Naphtha and Kerosene", containment: 205000, location: "H st and 3rd st", roof: "Cone/IFR" },
@@ -39,6 +39,7 @@ const tankData = {
   }
 
   export default tankData
+
 
 
 
